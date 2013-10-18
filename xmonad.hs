71ebf2bea6de47e72590e35895d8e8a004bdf0bb
Joel Kuiper <me@joelkuiper.eu>
2013-10-18 10:37:37 +0200

    Add fix for Java in Xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -1,1 +1,31 @@
-/home/joel/.xmonad/xmonad.hs
+import XMonad
+import XMonad.ManageHook
+import XMonad.Config.Gnome
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
+
+-- LAYOUTS
+import XMonad.Layout.NoBorders
+
+
+myManageHook = composeAll [
+    (className =? "Gnome-panel" <&&> title =? "Run Application") --> doCenterFloat
+  , (className =? "Xfce4-notifyd" --> doIgnore)
+  , (className =? "Do" --> doIgnore)
+  , (className =? "Cairo-dock" --> doIgnore)
+  , isFullscreen --> doFullFloat
+  , manageDocks
+   ]
+
+conf = gnomeConfig {
+    modMask = mod4Mask
+  , layoutHook = smartBorders (layoutHook gnomeConfig)
+  , handleEventHook = docksEventHook
+  , manageHook = myManageHook <+> manageHook gnomeConfig
+  }
+
+main = do
+    xmonad conf
+        { startupHook = startupHook conf >> setWMName "LG3D"
+        }
