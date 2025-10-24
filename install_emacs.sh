a5832aad0d1d608c61525700ca73de4acb7b71c0
Joël Kuiper <me@joelkuiper.eu>
2025-10-24 11:58:43 +0200

    Update install_emacs.sh

--- install_emacs.sh
+++ install_emacs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo apt install -y build-essential gcc-10  libgccjit-10-dev libxpm-dev libjpeg-dev libgif-dev libtiff-dev libgnutls28-dev libjson11-1-dev  gnutls-bin libjansson-dev libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo libtool-bin libtool libtree-sitter-dev libxml2-dev
+sudo apt install -y \
+  build-essential gcc-10 libgccjit-10-dev libxpm-dev libjpeg-dev libgif-dev \
+  libtiff-dev libgnutls28-dev libjson11-1-dev gnutls-bin libjansson-dev \
+  libgtk-3-dev libtiff5-dev libpng-dev libncurses-dev texinfo libtool-bin \
+  libtool libtree-sitter-dev libxml2-dev
 
 git clone --depth 1 --branch emacs-30.2 git@github.com:emacs-mirror/emacs.git
 
